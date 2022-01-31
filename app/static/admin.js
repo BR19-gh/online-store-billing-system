6d76313ecef4e603714ed5093455bf851086da85
Ibrahim Alkhowaiter <ibrahim-abdalaziz@hotmail.com>
2022-01-31 22:54:56 +0300

    adding imgs storing

--- app/static/admin.js
+++ app/static/admin.js
@@ -426,9 +426,9 @@ function firstFetch() {
             for (let i = 0; i < Object.keys(responseJson).length; i++) {
 
                 if (i == Object.keys(responseJson).length) {
-                    document.getElementById('productsList').innerHTML += `(${responseJson[i]['id']}, ${responseJson[i]['title']}, ${responseJson[i]['price']}, <img style="width: 50px; height: 50px; margin: 0;" src="/img/products_imgs/${responseJson[i]['imgName']}" alt="image name: ${responseJson[i]['imgName']}">)`;
+                    document.getElementById('productsList').innerHTML += `(${responseJson[i]['id']}, ${responseJson[i]['title']}, ${responseJson[i]['price']}, <img style="width: 50px; height: 50px; margin: 0;" src="data:image/png;base64,${responseJson[i]['imgName']}" alt="img">)`;
                 }
-                document.getElementById('productsList').innerHTML += `(${responseJson[i]['id']}, ${responseJson[i]['title']}, ${responseJson[i]['price']}, <img style="width: 50px; height: 50px; margin: 0;" src="/img/products_imgs/${responseJson[i]['imgName']}" alt="image name: ${responseJson[i]['imgName']}">)<br>`;
+                document.getElementById('productsList').innerHTML += `(${responseJson[i]['id']}, ${responseJson[i]['title']}, ${responseJson[i]['price']}, <img style="width: 50px; height: 50px; margin: 0;" src="data:image/png;base64,${responseJson[i]['imgName']}" alt="img">)<br>`;
             }
 
         });
