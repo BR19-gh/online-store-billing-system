8638a2ccfc57aaf888d583318e7ddc43b7c4f069
Ibrahim Alkhowaiter <ibrahim-abdalaziz@hotmail.com>
2022-02-06 21:22:36 +0300

    fixing some titles

--- app/static/main.js
+++ app/static/main.js
@@ -125,8 +125,8 @@ function firstFetch() {
     </div>
 `
                                     }
-                                    var allButtons = document.querySelectorAll('div[class^=addToCartBtn]');
-                                    console.log("Found", allButtons.length, 'div which class:"addToCartBtn".');
+                                    var allButtons = document.querySelectorAll('a[class^=addToCartBtn]');
+                                    console.log("Found", allButtons.length, 'a which class:"addToCartBtn".');
                                     for (var i = 0; i < allButtons.length; i++) {
                                         allButtons[i].addEventListener('click', function() {
                                             // to allow to enter promo code again
