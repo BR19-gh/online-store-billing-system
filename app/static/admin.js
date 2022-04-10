d7d4cfee30821ff3b0743507014f3114b219c746
Ibrahim Alkhowaiter <ibrahim-abdalaziz@hotmail.com>
2022-04-10 23:09:09 +0300

    new update!, changimg store themes ability

--- app/static/admin.js
+++ app/static/admin.js
@@ -39,7 +39,7 @@ for (var i = 0; i < themeBtns.length; i++) {
                             return;
                         }
                         // location.reload();
-                        fetch('/storeTheme/show', { method: 'GET', }).then((responseName) => { console.log(responseName.json()); })
+                        fetch('/storeTheme/show', { method: 'GET', }).then((responseName) => { return responseName.json(); }).then((responseJson) => { console.log(responseJson) })
                     });
             });
 
