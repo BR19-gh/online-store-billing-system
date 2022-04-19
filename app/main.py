407532b93bbf9c47d4b2de876e25d1a1062fd351
Ibrahim Alkhowaiter <ibrahim-abdalaziz@hotmail.com>
2022-04-19 06:20:35 +0300

    fixing catching errors

--- app/main.py
+++ app/main.py
@@ -457,13 +457,13 @@ def promocode(idIn=None):
         code = data['code']
         try:
             amount = int(data['amount'])/100
-            print(1)
+            print(111111111111111111111111111111)
         except:
-            print(2)
+            print(2222222222222222222222222222222)
             return jsonify({"msg": f"Bad Request 400: code was not added, even the provided amount is not float, or it contains illegal form of characters", "statCode": 400})
 
         try:
-            print(3)
+            print(33333333333333333333333333333)
             oldPrudRecord = newObj.search(idIn)
             newObj.update(idIn, code, amount)
 
@@ -473,7 +473,7 @@ def promocode(idIn=None):
             elif (recordSearched[0] == idIn):
                 return jsonify({"msg": f"Success 200: code_idIn::{idIn} is updated, old data:{oldPrudRecord}, new data:{newObj.search(idIn)}", "statCode": 200})
         except:
-            print(4)
+            print(44444444444444444444444444444444)
             if (isinstance(idIn, int) == False or isinstance(amount, float) == False):
                 return jsonify({"msg": f"Bad Request 400: code was not updated, even the provided idIn or amount are not integer/float, or they contain illegal form of characters", "statCode": 400})
             else:
