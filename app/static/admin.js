eb66f840ebcd83133c7e7a05f95ddb4d1e815873
Ibrahim Alkhowaiter <ibrahim-abdalaziz@hotmail.com>
2022-04-19 07:08:27 +0300

    fixing catching errors

--- app/static/admin.js
+++ app/static/admin.js
@@ -475,7 +475,7 @@ document.getElementById('updInfo').addEventListener('click', () => {
                 return response.json();
             }).then((responseJson) => {
                 if (responseJson.statCode == 400) {
-                    alert('فشلت الإضافة، هناك مدخلات أُدخلت بشكل خاطئ\nرقم المتجر أُدخل فيه نص، يجب إدخاله على شكل فقط. \n\n ErrCode: 400 : رمز الخطأ')
+                    alert('فشل التحديث، هناك مدخلات أُدخلت بشكل خاطئ\nرقم المتجر أُدخل فيه نص، يجب إدخاله على شكل فقط. \n\n ErrCode: 400 : رمز الخطأ')
                     return;
                 }
                 if (responseJson.statCode == 429) {
@@ -516,7 +516,7 @@ document.getElementById('updInfo').addEventListener('click', () => {
             return { num: responseNum.json(), name: responseName.json() };
         }).then((responseJson) => {
             if (responseJson['num'].statCode == 400) {
-                alert('فشلت الإضافة، هناك مدخلات أُدخلت بشكل خاطئ\nرقم المتجر أُدخل فيه نص، يجب إدخاله على شكل فقط. \n\n ErrCode: 400 : رمز الخطأ')
+                alert('فشل التحديث، هناك مدخلات أُدخلت بشكل خاطئ\nرقم المتجر أُدخل فيه نص، يجب إدخاله على شكل فقط. \n\n ErrCode: 400 : رمز الخطأ')
                 return;
             }
             if (responseJson.statCode == 429) {
