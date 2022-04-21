e89caf7aae5a28c0b20cf2c2f1516feeb716577f
Ibrahim Alkhowaiter <ibrahim-abdalaziz@hotmail.com>
2022-04-22 01:14:45 +0300

    adding the ablity to order outputs even after update

--- app/static/admin.js
+++ app/static/admin.js
@@ -560,10 +560,10 @@ function firstFetch() {
             for (let i = 0; i < Object.keys(responseJson).length; i++) {
 
                 if (i == Object.keys(responseJson).length) {
-                    document.getElementById('productsList').innerHTML += `<div style="display: flex; justify-content: space-evenly; color: #4b4b4b;"><b>${responseJson[i]['id']}</b><b>${responseJson[i]['title']}</b><b>${responseJson[i]['price']}﷼</b><b><img style="border: 1px solid #8f8d85 ; border-radius: 10px; width: 30px; height: 30px; margin: 0;" src="data:image/png;base64,${responseJson[i]['img']}" alt="img"></b></div>`;
+                    document.getElementById('productsList').innerHTML += `<div style="display: flex; justify-content: space-evenly; color: #4b4b4b;"><b>${responseJson[Object.keys(responseJson)[i]]['id']}</b><b>${responseJson[Object.keys(responseJson)[i]]['title']}</b><b>${responseJson[Object.keys(responseJson)[i]]['price']}﷼</b><b><img style="border: 1px solid #8f8d85 ; border-radius: 10px; width: 30px; height: 30px; margin: 0;" src="data:image/png;base64,${responseJson[Object.keys(responseJson)[i]]['img']}" alt="img"></b></div>`;
                 }
 
-                document.getElementById('productsList').innerHTML += `<div style="display: flex; justify-content: space-evenly; color: #4b4b4b;"><b>${responseJson[i]['id']}</b><b>${responseJson[i]['title']}</b><b>${responseJson[i]['price']}﷼</b><b><img style="border: 1px solid #8f8d85 ; border-radius: 10px; width: 30px; height: 30px; margin: 0;" src="data:image/png;base64,${responseJson[i]['img']}" alt="img"></b></div><br>`;
+                document.getElementById('productsList').innerHTML += `<div style="display: flex; justify-content: space-evenly; color: #4b4b4b;"><b>${responseJson[Object.keys(responseJson)[Object.keys(responseJson)[i]]]['id']}</b><b>${responseJson[Object.keys(responseJson)[i]]['title']}</b><b>${responseJson[Object.keys(responseJson)[i]]['price']}﷼</b><b><img style="border: 1px solid #8f8d85 ; border-radius: 10px; width: 30px; height: 30px; margin: 0;" src="data:image/png;base64,${responseJson[Object.keys(responseJson)[i]]['img']}" alt="img"></b></div><br>`;
             }
 
         });
@@ -590,9 +590,9 @@ function firstFetch() {
             for (let i = 0; i < Object.keys(responseJson).length; i++) {
 
                 if (i == Object.keys(responseJson).length) {
-                    document.getElementById('codesList').innerHTML += `<div style="display: flex; justify-content: space-evenly; color: #4b4b4b;"><b>${responseJson[i]['id']}</b><b>${responseJson[i]['code']}</b><b>${responseJson[i]['amount']*100}%</b></div>`;
+                    document.getElementById('codesList').innerHTML += `<div style="display: flex; justify-content: space-evenly; color: #4b4b4b;"><b>${responseJson[Object.keys(responseJson)[i]]['id']}</b><b>${responseJson[Object.keys(responseJson)[i]]['code']}</b><b>${responseJson[Object.keys(responseJson)[i]]['amount']*100}%</b></div>`;
                 }
-                document.getElementById('codesList').innerHTML += `<div style="display: flex; justify-content: space-evenly; color: #4b4b4b;"><b>${responseJson[i]['id']}</b><b>${responseJson[i]['code']}</b><b>${responseJson[i]['amount']*100}%</b></div><br>`;
+                document.getElementById('codesList').innerHTML += `<div style="display: flex; justify-content: space-evenly; color: #4b4b4b;"><b>${responseJson[Object.keys(responseJson)[i]]['id']}</b><b>${responseJson[Object.keys(responseJson)[i]]['code']}</b><b>${responseJson[Object.keys(responseJson)[i]]['amount']*100}%</b></div><br>`;
             }
         });
 
