5261a6612ff06acdc3aa4825b8cdd38b82de6159
Ibrahim Alkhowaiter <ibrahim-abdalaziz@hotmail.com>
2022-06-01 00:34:18 +0300

    using switch-case statment instead of if statment in themeFetches

--- app/static/admin.js
+++ app/static/admin.js
@@ -118,116 +118,129 @@ function fetchThemes() {
             return responseName.json();
         })
         .then((responseJson) => {
+            let setProp = document.documentElement.style.setProperty;
+            if (responseJson['storeTheme'] != "none/لايوجد") {
+                switch (responseJson['storeTheme']) {
+                    case 'originalTheme':
+                        setProp('--body', '#f0e9c9');
+                        setProp('--borders', '#8f8d85');
+                        setProp('--btns', '#f5ebbd');
+                        setProp('--btnsHover', '#bbb496');
+                        setProp('--container', '#d4cbabc0');
+                        setProp('--containerHover', '#d4cbabb0');
+                        setProp('--h1', '#c2bba1a0');
+                        setProp('--h1OtherScreen', '#c2bba1d0');
+                        setProp('--form', '#c4c1b6c0');
+                        setProp('--formHover', '#c4c1b6');
+                        setProp('--input', '#8f8d85');
+                        setProp('--inputHover', '#aca588');
+                        setProp('--inputFocus', '#b1a883');
+                        setProp('--webkitScrollbarTrack', '#c4c1b6c0');
+                        setProp('--webkitScrollbarThumb', '#969287');
+                        setProp('--webkitScrollbarThumbHover', '#797464');
+                        setProp('--infoCard', '#d4cbaba8');
+                        break;
 
-            if (responseJson['storeTheme'] == "none/لايوجد") {} else {
-                if (responseJson['storeTheme'] == 'originalTheme') {
-                    document.documentElement.style.setProperty('--body', '#f0e9c9');
-                    document.documentElement.style.setProperty('--borders', '#8f8d85');
-                    document.documentElement.style.setProperty('--btns', '#f5ebbd');
-                    document.documentElement.style.setProperty('--btnsHover', '#bbb496');
-                    document.documentElement.style.setProperty('--container', '#d4cbabc0');
-                    document.documentElement.style.setProperty('--containerHover', '#d4cbabb0');
-                    document.documentElement.style.setProperty('--h1', '#c2bba1a0');
-                    document.documentElement.style.setProperty('--h1OtherScreen', '#c2bba1d0');
-                    document.documentElement.style.setProperty('--form', '#c4c1b6c0');
-                    document.documentElement.style.setProperty('--formHover', '#c4c1b6');
-                    document.documentElement.style.setProperty('--input', '#8f8d85');
-                    document.documentElement.style.setProperty('--inputHover', '#aca588');
-                    document.documentElement.style.setProperty('--inputFocus', '#b1a883');
-                    document.documentElement.style.setProperty('--webkitScrollbarTrack', '#c4c1b6c0');
-                    document.documentElement.style.setProperty('--webkitScrollbarThumb', '#969287');
-                    document.documentElement.style.setProperty('--webkitScrollbarThumbHover', '#797464');
-                    document.documentElement.style.setProperty('--infoCard', '#d4cbaba8');
-                } else if (responseJson['storeTheme'] == 'blueTheme') {
-                    document.documentElement.style.setProperty('--body', '#c9cbf0');
-                    document.documentElement.style.setProperty('--borders', '#85868f');
-                    document.documentElement.style.setProperty('--btns', '#bdbef5');
-                    document.documentElement.style.setProperty('--btnsHover', '#8987ba');
-                    document.documentElement.style.setProperty('--container', '#4169E1c0');
-                    document.documentElement.style.setProperty('--containerHover', '#4169E1b0');
-                    document.documentElement.style.setProperty('--h1', '#a3a1c2a0');
-                    document.documentElement.style.setProperty('--h1OtherScreen', '#a3a1c2d0');
-                    document.documentElement.style.setProperty('--form', '#9696c1c0');
-                    document.documentElement.style.setProperty('--formHover', '#9696c1');
-                    document.documentElement.style.setProperty('--input', '#85858f');
-                    document.documentElement.style.setProperty('--inputHover', '#8889ac');
-                    document.documentElement.style.setProperty('--inputFocus', '#8783b1');
-                    document.documentElement.style.setProperty('--webkitScrollbarTrack', '#b6b6c4c0');
-                    document.documentElement.style.setProperty('--webkitScrollbarThumb', '#878796');
-                    document.documentElement.style.setProperty('--webkitScrollbarThumbHover', '#646579');
-                    document.documentElement.style.setProperty('--infoCard', '#abaed4a8');
-                } else if (responseJson['storeTheme'] == 'greenTheme') {
-                    document.documentElement.style.setProperty('--body', '#c9f0cb');
-                    document.documentElement.style.setProperty('--borders', '#858f86');
-                    document.documentElement.style.setProperty('--btns', '#bdf5be');
-                    document.documentElement.style.setProperty('--btnsHover', '#96bb9a');
-                    document.documentElement.style.setProperty('--container', '#1e796bc0');
-                    document.documentElement.style.setProperty('--containerHover', '#1e796bb0');
-                    document.documentElement.style.setProperty('--h1', '#a1c2a3a0');
-                    document.documentElement.style.setProperty('--h1OtherScreen', '#a1c2a3d0');
-                    document.documentElement.style.setProperty('--form', '#b6c4b7c0');
-                    document.documentElement.style.setProperty('--formHover', '#b6c4b8');
-                    document.documentElement.style.setProperty('--input', '#858f85');
-                    document.documentElement.style.setProperty('--inputHover', '#88ac8b');
-                    document.documentElement.style.setProperty('--inputFocus', '#83b185');
-                    document.documentElement.style.setProperty('--webkitScrollbarTrack', '#b6c4b8c0');
-                    document.documentElement.style.setProperty('--webkitScrollbarThumb', '#879687');
-                    document.documentElement.style.setProperty('--webkitScrollbarThumbHover', '#647965');
-                    document.documentElement.style.setProperty('--infoCard', '#abd4b0a8');
-                } else if (responseJson['storeTheme'] == 'redTheme') {
-                    document.documentElement.style.setProperty('--body', '#f0c9c9');
-                    document.documentElement.style.setProperty('--borders', '#8f8585');
-                    document.documentElement.style.setProperty('--btns', '#f5bdbd');
-                    document.documentElement.style.setProperty('--btnsHover', '#bb9696');
-                    document.documentElement.style.setProperty('--container', '#ad5a54c0');
-                    document.documentElement.style.setProperty('--containerHover', '#ad5a54b0');
-                    document.documentElement.style.setProperty('--h1', '#c2a1a1a0');
-                    document.documentElement.style.setProperty('--h1OtherScreen', '#c2a1a1d0');
-                    document.documentElement.style.setProperty('--form', '#c4b6b6c0');
-                    document.documentElement.style.setProperty('--formHover', '#c4b6b6');
-                    document.documentElement.style.setProperty('--input', '#8f8585');
-                    document.documentElement.style.setProperty('--inputHover', '#ac8888');
-                    document.documentElement.style.setProperty('--inputFocus', '#b18383');
-                    document.documentElement.style.setProperty('--webkitScrollbarTrack', '#c4b6b6c0');
-                    document.documentElement.style.setProperty('--webkitScrollbarThumb', '#968787');
-                    document.documentElement.style.setProperty('--webkitScrollbarThumbHover', '#796464');
-                    document.documentElement.style.setProperty('--infoCard', '#d4ababa8');
-                } else if (responseJson['storeTheme'] == 'pinkTheme') {
-                    document.documentElement.style.setProperty('--body', '#efc9f0');
-                    document.documentElement.style.setProperty('--borders', '#8f858f');
-                    document.documentElement.style.setProperty('--btns', '#f3bdf5');
-                    document.documentElement.style.setProperty('--btnsHover', '#bb96bb');
-                    document.documentElement.style.setProperty('--container', '#ad5498c0');
-                    document.documentElement.style.setProperty('--containerHover', '#ad5498b0');
-                    document.documentElement.style.setProperty('--h1', '#c2a1b5d0');
-                    document.documentElement.style.setProperty('--h1OtherScreen', '#c4b6c2c0');
-                    document.documentElement.style.setProperty('--form', '#c4b6c4');
-                    document.documentElement.style.setProperty('--formHover', '#c4b6c4');
-                    document.documentElement.style.setProperty('--input', '#8f858f');
-                    document.documentElement.style.setProperty('--inputHover', '#ac88a9');
-                    document.documentElement.style.setProperty('--inputFocus', '#b183aa');
-                    document.documentElement.style.setProperty('--webkitScrollbarTrack', '#c4b6c1c0');
-                    document.documentElement.style.setProperty('--webkitScrollbarThumb', '#968795');
-                    document.documentElement.style.setProperty('--webkitScrollbarThumbHover', '#796478');
-                    document.documentElement.style.setProperty('--infoCard', '#d4abcaa8');
-                } else {
-                    document.documentElement.style.setProperty('--body', '#bbc0c6');
-                    document.documentElement.style.setProperty('--borders', '#888888');
-                    document.documentElement.style.setProperty('--btns', '#a7aaad');
-                    document.documentElement.style.setProperty('--btnsHover', '#94979b');
-                    document.documentElement.style.setProperty('--container', '#838690c0');
-                    document.documentElement.style.setProperty('--containerHover', '#838690b0');
-                    document.documentElement.style.setProperty('--h1', '#c2c2c2a0');
-                    document.documentElement.style.setProperty('--h1OtherScreen', '#c2c2c2d0');
-                    document.documentElement.style.setProperty('--form', '#cdcdcec0');
-                    document.documentElement.style.setProperty('--formHover', '#cdcdce');
-                    document.documentElement.style.setProperty('--input', '#8a8a8a');
-                    document.documentElement.style.setProperty('--inputHover', '#a9a9a9');
-                    document.documentElement.style.setProperty('--inputFocus', '#aeaeae');
-                    document.documentElement.style.setProperty('--webkitScrollbarTrack', '#b6b6c4c0');
-                    document.documentElement.style.setProperty('--webkitScrollbarThumb', '#969696');
-                    document.documentElement.style.setProperty('--webkitScrollbarThumbHover', '#727272');
-                    document.documentElement.style.setProperty('--infoCard', '#d1d1d1a8');
+                    case 'blueTheme':
+                        setProp('--body', '#c9cbf0');
+                        setProp('--borders', '#85868f');
+                        setProp('--btns', '#bdbef5');
+                        setProp('--btnsHover', '#8987ba');
+                        setProp('--container', '#4169E1c0');
+                        setProp('--containerHover', '#4169E1b0');
+                        setProp('--h1', '#a3a1c2a0');
+                        setProp('--h1OtherScreen', '#a3a1c2d0');
+                        setProp('--form', '#9696c1c0');
+                        setProp('--formHover', '#9696c1');
+                        setProp('--input', '#85858f');
+                        setProp('--inputHover', '#8889ac');
+                        setProp('--inputFocus', '#8783b1');
+                        setProp('--webkitScrollbarTrack', '#b6b6c4c0');
+                        setProp('--webkitScrollbarThumb', '#878796');
+                        setProp('--webkitScrollbarThumbHover', '#646579');
+                        setProp('--infoCard', '#abaed4a8');
+                        break;
+
+                    case 'greenTheme':
+                        setProp('--body', '#c9f0cb');
+                        setProp('--borders', '#858f86');
+                        setProp('--btns', '#bdf5be');
+                        setProp('--btnsHover', '#96bb9a');
+                        setProp('--container', '#1e796bc0');
+                        setProp('--containerHover', '#1e796bb0');
+                        setProp('--h1', '#a1c2a3a0');
+                        setProp('--h1OtherScreen', '#a1c2a3d0');
+                        setProp('--form', '#b6c4b7c0');
+                        setProp('--formHover', '#b6c4b8');
+                        setProp('--input', '#858f85');
+                        setProp('--inputHover', '#88ac8b');
+                        setProp('--inputFocus', '#83b185');
+                        setProp('--webkitScrollbarTrack', '#b6c4b8c0');
+                        setProp('--webkitScrollbarThumb', '#879687');
+                        setProp('--webkitScrollbarThumbHover', '#647965');
+                        setProp('--infoCard', '#abd4b0a8');
+                        break;
+
+                    case 'redTheme':
+                        setProp('--body', '#f0c9c9');
+                        setProp('--borders', '#8f8585');
+                        setProp('--btns', '#f5bdbd');
+                        setProp('--btnsHover', '#bb9696');
+                        setProp('--container', '#ad5a54c0');
+                        setProp('--containerHover', '#ad5a54b0');
+                        setProp('--h1', '#c2a1a1a0');
+                        setProp('--h1OtherScreen', '#c2a1a1d0');
+                        setProp('--form', '#c4b6b6c0');
+                        setProp('--formHover', '#c4b6b6');
+                        setProp('--input', '#8f8585');
+                        setProp('--inputHover', '#ac8888');
+                        setProp('--inputFocus', '#b18383');
+                        setProp('--webkitScrollbarTrack', '#c4b6b6c0');
+                        setProp('--webkitScrollbarThumb', '#968787');
+                        setProp('--webkitScrollbarThumbHover', '#796464');
+                        setProp('--infoCard', '#d4ababa8');
+                        break;
+
+                    case 'pinkTheme':
+                        setProp('--body', '#efc9f0');
+                        setProp('--borders', '#8f858f');
+                        setProp('--btns', '#f3bdf5');
+                        setProp('--btnsHover', '#bb96bb');
+                        setProp('--container', '#ad5498c0');
+                        setProp('--containerHover', '#ad5498b0');
+                        setProp('--h1', '#c2a1b5d0');
+                        setProp('--h1OtherScreen', '#c4b6c2c0');
+                        setProp('--form', '#c4b6c4');
+                        setProp('--formHover', '#c4b6c4');
+                        setProp('--input', '#8f858f');
+                        setProp('--inputHover', '#ac88a9');
+                        setProp('--inputFocus', '#b183aa');
+                        setProp('--webkitScrollbarTrack', '#c4b6c1c0');
+                        setProp('--webkitScrollbarThumb', '#968795');
+                        setProp('--webkitScrollbarThumbHover', '#796478');
+                        setProp('--infoCard', '#d4abcaa8');
+                        break;
+
+                    default:
+                        setProp('--body', '#bbc0c6');
+                        setProp('--borders', '#888888');
+                        setProp('--btns', '#a7aaad');
+                        setProp('--btnsHover', '#94979b');
+                        setProp('--container', '#838690c0');
+                        setProp('--containerHover', '#838690b0');
+                        setProp('--h1', '#c2c2c2a0');
+                        setProp('--h1OtherScreen', '#c2c2c2d0');
+                        setProp('--form', '#cdcdcec0');
+                        setProp('--formHover', '#cdcdce');
+                        setProp('--input', '#8a8a8a');
+                        setProp('--inputHover', '#a9a9a9');
+                        setProp('--inputFocus', '#aeaeae');
+                        setProp('--webkitScrollbarTrack', '#b6b6c4c0');
+                        setProp('--webkitScrollbarThumb', '#969696');
+                        setProp('--webkitScrollbarThumbHover', '#727272');
+                        setProp('--infoCard', '#d1d1d1a8');
+                        break;
+
                 }
             }
             console.log('current theme is ' + responseJson['storeTheme'])
