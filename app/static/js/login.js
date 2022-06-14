ececfa1822d73bf6923bc2e8fffe2ffa71321029
Ibrahim Alkhowaiter <ibrahim-abdalaziz@hotmail.com>
2022-06-14 20:37:18 +0300

    formatting

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,24 +1,35 @@
 function firstFetch() {
-    fetch("/storeName/show").then((response) => {
-        return response.json()
-    }).then((responseJson) => {
-        if (responseJson.storeName == '') document.querySelector('#storeName').innerText = ''
-        else document.querySelector('#storeName').innerText = `${responseJson.storeName}`
-    }).catch(error => {
-        alert(`هناك خطأ في التواصل مع السيرفر، تواصل مع المطور لحل المشكلة أو انتظر حتى يتم حلها\nالخطأ: ${error}//login:storeName// 506\n التاريخ: ${formatDate(new Date())}`)
-    });
-
-
-
+    fetch('/storeName/show')
+        .then((response) => {
+            return response.json()
+        })
+        .then((responseJson) => {
+            if (responseJson.storeName == '')
+                document.querySelector('#storeName').innerText = ''
+            else
+                document.querySelector(
+                    '#storeName'
+                ).innerText = `${responseJson.storeName}`
+        })
+        .catch((error) => {
+            alert(
+                `هناك خطأ في التواصل مع السيرفر، تواصل مع المطور لحل المشكلة أو انتظر حتى يتم حلها\nالخطأ: ${error}//login:storeName// 506\n التاريخ: ${formatDate(
+                    new Date()
+                )}`
+            )
+        })
 }
 
-firstFetch();
+firstFetch()
 
 function login() {
-    location.assign(`/verify/${document.querySelector('#username').value}/${document.querySelector('#password').value}`)
-
+    location.assign(
+        `/verify/${document.querySelector('#username').value}/${
+            document.querySelector('#password').value
+        }`
+    )
 }
 document.querySelector('#loginBtn').addEventListener('click', (event) => {
-    login();
-    event.preventDefault();
+    login()
+    event.preventDefault()
 })
