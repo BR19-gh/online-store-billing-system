6c2479d4c08003c3c0f99d9b37a879504b1d2309
Ibrahim Alkhowaiter <ibrahim-abdalaziz@hotmail.com>
2022-06-18 19:44:58 +0300

    (gamma ver3.0) gradually converting JS code to TS

---
+++ app/static/ts/login.ts
@@ -0,0 +1,37 @@
+let storeName = <HTMLElement> document.querySelector('#storeName');
+function firstFetch() : void {
+    fetch('/storeName/show')
+        .then((response) => {
+            return response.json()
+        })
+        .then((responseJson) => {
+            if (responseJson.storeName == '') {
+               
+               storeName!.innerText = ''
+            } else {
+                storeName!.innerText = `${responseJson.storeName}`
+            }
+        })
+        .catch((error) => {
+            alert(
+                `توجد مشكلة في التواصل مع السيرفر،\nحاول مجددًا في وقت لاحق، إذا استمرت المشكلة، تواصل مع المطور. \n\n ErrCode: ${error}//login:storeName// 506\n التاريخ: ${formatDate(
+                    new Date()
+                )}`
+            )
+        })
+}
+
+firstFetch()
+let username = <HTMLInputElement> document.querySelector('#username');
+let password = <HTMLInputElement> document.querySelector('#password');
+function login(): void {
+    location.assign(
+        `/verify/${username!.value}/${
+            password!.value
+        }`
+    )
+}
+document.querySelector('#loginBtn')!.addEventListener('click', (event) => {
+    login()
+    event.preventDefault()
+})
