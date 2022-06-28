a75664c7f3ed0971f715fcba5b0ecf2789aee0b7
Ibrahim Alkhowaiter <ibrahim-abdalaziz@hotmail.com>
2022-06-28 17:38:44 +0300

    (gamma ver3.0) using post in login

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -5,7 +5,7 @@ function firstFetch() {
             return response.json();
         })
         .then((responseJson) => {
-            if (responseJson.storeName == "") {
+            if (responseJson.storeName === "") {
                 document.querySelector("#storeName").innerText = "";
             } else {
                 document.querySelector(
@@ -49,3 +49,23 @@ document.querySelector("#loginBtn").addEventListener("click", (event) => {
     login();
     event.preventDefault();
 });
+
+function formatDate(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    const ampm = hours >= 12 ? "م" : "ص";
+    hours = hours % 12;
+    hours = hours || 12;
+    // minutes = minutes < 10 ? '0' + minutes : minutes;
+    const year = date.getFullYear();
+    let month = date.getMonth();
+    month =
+        Number(month) + 1 < 10 ? "0" + (Number(month) + 1) : Number(month) + 1;
+    var date = date.getDate();
+    date = date < 10 ? "0" + date : date;
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    const strTime =
+        year + "/" + month + "/" + date + ", " + hours + ":" + minutes + ampm;
+    return strTime;
+}
