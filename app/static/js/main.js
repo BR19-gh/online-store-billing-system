ed8b8c61e9b7b85e530780e4cee7606b67877a32
Ibrahim Alkhowaiter <ibrahim-abdalaziz@hotmail.com>
2022-08-30 02:50:20 +0300

    (ver3.5) adding ability to store and show past bills

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1308,7 +1308,7 @@ function firstFetch() {
                                                             );
                                                             return;
                                                         }
-                                                        console.log(billId);
+                                                        console.log(typeof billId);
                                                             // fetch("/billingHistory", {
                                                             //     headers: {
                                                             //         Method: "POST",
